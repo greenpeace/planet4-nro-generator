f38242c6cba092fca39ac4df9df5f707d90b5024
Raymond Walker <raymond.walker@greenpeace.org>
2018-05-15 19:09:44 +1000

    Verbose bucket creation text

--- bin/create_stateless_bucket.sh
+++ bin/create_stateless_bucket.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
+echo "Initialising WP Stateless bucket"
+echo ""
+echo "Project: ${PROJECT}"
+echo "Bucket:  gs://${BUCKET}"
+echo ""
+
 gsutil ls -p "${PROJECT}" "gs://${BUCKET}" || gsutil mb -p "${PROJECT}" "gs://${BUCKET}"
 
 gsutil iam ch allUsers:objectViewer "gs://${BUCKET}"
