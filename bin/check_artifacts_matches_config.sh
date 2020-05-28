beddc647886f1db64ea04b954eef8d1dbf6b1071
inwerpsel <pieter.vincent1988@gmail.com>
2020-05-28 19:43:27 +0200

    PLANET-5125 Add script to update config
    * Based on https://github.com/greenpeace/planet4-circleci-config/blob/6869c0b1754a13521676fa3b9f248472486fa5b3/create_configs.sh.
    So the some of this commit message mainly applies to the changes made
    to that script.
    * Use dockerize and same template as NRO generator to create the new
    config file.
    * Don't push to Github unless specified in the arguments. Otherwise
    still create the tmp config file but don't remove it.
    * Put site update logic in a function.
    * Add list for develop only sites.
    * Add arguments whether to include dev and/or prod.
    * Add script to check if config.yml contains any differences compared to
    artifacts.yml, which is the file containing the environment variables.
    We should probably get rid of the duplication of these variables, as now
    you're able to change config.yml without applying the same changes to
    artifacts.yml. If we wouldn't check for this when we push new
    configuration to all repos, these changes would be overwritten.
    * List sites in separate files: dev_sites.txt, test_prod_sites.txt and
    prod_sites.txt

---
+++ bin/check_artifacts_matches_config.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -eu
+
+function file_contained_in_other_file() {
+  local needle=$1
+  local haystack=$2
+
+  if grep --quiet "$(tr '\n' ' ' < "$needle")" <<< "$(tr '\n' ' ' < "$haystack")"
+  then
+    true
+  else
+    false
+  fi
+}
+
+readarray -t dev_sites < dev_sites.txt
+readarray -t test_prod_sites < test_prod_sites.txt
+readarray -t prod_sites < prod_sites.txt
+
+sites=("${dev_sites[@]}" "${test_prod_sites[@]}" "${prod_sites[@]}")
+
+all_valid=true
+tmp_dir=tmp/detect
+mkdir -p $tmp_dir
+
+for site in "${sites[@]}"
+do
+  repo="planet4-${site}"
+  rm -rf "${tmp_dir:?}/${repo}"
+  git -C ${tmp_dir} clone https://github.com/greenpeace/"${repo}" --quiet --single-branch --branch develop
+
+  if ! file_contained_in_other_file "$tmp_dir/$repo/.circleci/artifacts.yml" "$tmp_dir/$repo/.circleci/config.yml"
+  then
+    echo "${repo} ❌ artifacts.yml file is out of sync. Please check and resolves differences."
+    all_valid=false
+  else
+    echo "${repo} ✅"
+  fi
+done
+
+if [ "${all_valid}" = true ]
+then
+  echo "All files in sync."
+  exit 0;
+else
+  echo "Detected out of sync files."
+  exit 1;
+fi
+
---
+++ bin/update_configs.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+set -eu
+
+should_push=${1:-false}
+update_dev=${2:-true}
+update_prod=${3:-false}
+
+# Following sites have only a develop environment.
+readarray -t all_dev_sites < ./dev_sites.txt
+
+# Following sites have develop, release and production environment.
+readarray -t test_prod_sites < ./test_prod_sites.txt
+readarray -t actual_prod_sites < ./test_prod_sites.txt
+
+all_prod_sites=("${test_prod_sites[@]}" "${actual_prod_sites[@]}")
+
+config_header=./templates/nro/.circleci/config-header.yml.tmpl
+
+function update_site() {
+  local repo="planet4-$1"
+  local is_prod=${2:-false}
+  local tmp_config_file="./tmpConfig-$1"
+  local generator_hash
+  generator_hash="$(git rev-parse HEAD)"
+  local generator_link="https://github.com/greenpeace/planet4-nro-generator/blob/${generator_hash}/templates/nro/.circleci/config.yml.tmpl"
+
+  git clone https://github.com/greenpeace/"${repo}" --single-branch --branch develop --quiet
+
+  if [ ! -f "${repo}"/.circleci/artifacts.yml ]
+  then
+      echo "${repo} - DOES NOT have an artifacts file. Cannot generate new configuration"
+  else
+      # Actually dockerize is not a good tool for this script as it grabs everything from the current shell's
+      # environment, which it does because it's intended to be used in a docker image entrypoint. It would be safer if
+      # we used a template tool that doesn't do that.
+      IS_CONFIG_UPDATE=true \
+      MAKE_RELEASE=$is_prod \
+      MAKE_MASTER=$is_prod \
+      dockerize \
+      -template "./templates/nro/.circleci/config.yml.tmpl:${tmp_config_file}" \
+      -template ${config_header}:"${repo}/config_header.yml"
+
+      cat "${repo}/config_header.yml" "${repo}/.circleci/artifacts.yml" "${tmp_config_file}" > "${repo}/.circleci/config.yml"
+
+      if git -C "${repo}" diff --quiet
+      then
+          echo "${repo} - No changes needed"
+      else
+          # If specified push the updated config to the repos.
+          if [ "$should_push" = true ]
+          then
+            git -C "${repo}" commit -m "New CircleCI conf from ${generator_link}" .circleci/config.yml
+            git -C "${repo}" push
+
+            echo "${repo} - Generated and pushed new configuration"
+          fi
+      fi
+      rm "$tmp_config_file"
+  fi
+
+  # Only remove repo when
+  if [ "$should_push" = true ]
+  then
+    echo "${repo} - Removing git repo."
+    rm -rf "${repo}"
+  else
+    echo "${repo} - Dry run, preserving git repo."
+  fi
+
+}
+
+echo "First checking all artifacts.yml files."
+if ! ./bin/check_artifacts_matches_config.sh
+then
+  echo 'Some repositories have changes to config.yml that are not in artifacts.yml, aborting as otherwise these changes would be overwritten.'
+  exit 1;
+fi
+
+if [ ! "$update_dev" = true ]
+then
+  echo 'Skipping dev sites.'
+else
+  for SITE in "${all_dev_sites[@]}"
+  do
+    update_site "${SITE}" false
+  done
+fi
+
+if [ ! "$update_prod" = true ]
+then
+  echo 'Skipping prod sites.'
+else
+  for SITE in "${all_prod_sites[@]}"
+  do
+    update_site "${SITE}" true
+  done
+fi
