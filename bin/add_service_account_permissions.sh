77872c886974ef30e7daf1a1a8f191b00e57c80a
Foppe Pieters <foppe.pieters@greenpeace.org>
2023-09-28 17:27:52 +0200

    fix: add task to init sa before bucket creation

---
+++ bin/add_service_account_permissions.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+echo
+echo "========================================================================="
+echo
+echo "Adding storage.admin permission for ${SERVICE_ACCOUNT_NAME}"
+echo
+
+user=$(gcloud auth list --format=json | jq -r '.[] | select(.account|test("^.+?@greenpeace.org")) | .account')
+gcloud config set account "$user"
+
+service_account=$SERVICE_ACCOUNT_NAME@${GCP_DEVELOPMENT_PROJECT}.iam.gserviceaccount.com
+# this access is removed at the end of the script
+gcloud projects add-iam-policy-binding "${GCP_DEVELOPMENT_PROJECT}" \
+  --member="serviceAccount:$service_account" \
+  --role roles/storage.admin
+
+gcloud projects add-iam-policy-binding "${GCP_PRODUCTION_PROJECT}" \
+  --member="serviceAccount:$service_account" \
+  --role roles/storage.admin
