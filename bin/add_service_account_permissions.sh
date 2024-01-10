676362fb30dfef96315105e238676d87d35380b7
Foppe Pieters <foppe.pieters@greenpeace.org>
2024-01-10 15:51:53 +0100

    fix: make linting happy

--- bin/add_service_account_permissions.sh
+++ bin/add_service_account_permissions.sh
@@ -7,7 +7,7 @@ echo
 echo "Adding storage.admin permission for ${SERVICE_ACCOUNT_NAME}"
 echo
 
-user=$(gcloud auth list --format=json | jq -r '.[] | select(.account|test("^.+?@greenpeace.org")) | .account') | sed -n '1{p;q}' 
+user=$(gcloud auth list --format=json | jq -r '.[] | select(.account|test("^.+?@greenpeace.org")) | .account' | sed -n '1{p;q}')
 gcloud config set account "$user"
 
 service_account=$SERVICE_ACCOUNT_NAME@${GCP_DEVELOPMENT_PROJECT}.iam.gserviceaccount.com
--- bin/clean_service_account_permissions.sh
+++ bin/clean_service_account_permissions.sh
@@ -9,10 +9,10 @@ echo
 
 gcloud auth revoke "$SERVICE_ACCOUNT_NAME@${GCP_DEVELOPMENT_PROJECT}.iam.gserviceaccount.com" || true
 
-user=$(gcloud auth list --format=json | jq -r '.[] | select(.account|test("^.+?@greenpeace.org")) | .account') | sed -n '1{p;q}' 
+user=$(gcloud auth list --format=json | jq -r '.[] | select(.account|test("^.+?@greenpeace.org")) | .account' | sed -n '1{p;q}')
 gcloud config set account "$user"
 
-# comment out for now since backup jobs need this
+# commenty out for now since backup jobs need this
 # service_account=$SERVICE_ACCOUNT_NAME@${GCP_DEVELOPMENT_PROJECT}.iam.gserviceaccount.com
 
 # gcloud projects remove-iam-policy-binding "${GCP_DEVELOPMENT_PROJECT}" \
