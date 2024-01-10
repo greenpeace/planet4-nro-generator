f857ba981c6615fc5e36d31a0565d6463dd038ae
Foppe Pieters <foppe.pieters@greenpeace.org>
2024-01-10 15:53:18 +0100

    fix: typo

--- bin/clean_service_account_permissions.sh
+++ bin/clean_service_account_permissions.sh
@@ -12,7 +12,7 @@ gcloud auth revoke "$SERVICE_ACCOUNT_NAME@${GCP_DEVELOPMENT_PROJECT}.iam.gservic
 user=$(gcloud auth list --format=json | jq -r '.[] | select(.account|test("^.+?@greenpeace.org")) | .account' | sed -n '1{p;q}')
 gcloud config set account "$user"
 
-# commenty out for now since backup jobs need this
+# comment out for now since backup jobs need this
 # service_account=$SERVICE_ACCOUNT_NAME@${GCP_DEVELOPMENT_PROJECT}.iam.gserviceaccount.com
 
 # gcloud projects remove-iam-policy-binding "${GCP_DEVELOPMENT_PROJECT}" \
